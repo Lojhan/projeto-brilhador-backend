6a6873a03a1e0bbdd2e1807d30d255274e12a172
ViniciusLisboa07 <viniciuslisboa1001@gmail.com>
2022-05-17 21:34:26 -0300

    crudReposity

--- supply-chain/src/main/java/com/supplyChain/supplyChainProject/repositories/MovementRepository.java
+++ supply-chain/src/main/java/com/supplyChain/supplyChainProject/repositories/MovementRepository.java
@@ -6,17 +6,12 @@ import java.util.UUID;
 
 import com.supplyChain.supplyChainProject.models.Movement;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MovementRepository extends JpaRepository<Movement, Long> {
-    
-    @Query("select m from Movements as m")
-    public List<Movement> findAll();
+public interface MovementRepository extends CrudRepository<Movement, Long> {
 
-    @Query("select m from Movements as m where m.id = ?1")
-    public Optional<Movement[]> findById(UUID userId);
+
 }
