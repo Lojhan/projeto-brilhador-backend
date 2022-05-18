2610b071ca491322ec6d6baff9110d91c028ec06
ViniciusLisboa07 <viniciuslisboa1001@gmail.com>
2022-05-17 21:25:54 -0300

    movemet repository

--- supply-chain/src/main/java/com/supplyChain/supplyChainProject/repositories/MovementRepository.java
+++ supply-chain/src/main/java/com/supplyChain/supplyChainProject/repositories/MovementRepository.java
@@ -1,12 +1,22 @@
 package com.supplyChain.supplyChainProject.repositories;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import com.supplyChain.supplyChainProject.models.Movement;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MovementRepository extends JpaRepository<Movement, Long> {
     
+    @Query("select m from Movements as m")
+    public List<Movement> findAll();
+
+    @Query("select m from Movements as m where m.id = ?1")
+    public Optional<Movement[]> findById(UUID userId);
 }
