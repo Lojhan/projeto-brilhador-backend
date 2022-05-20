c6a3f83853471b13a3117b5fcb17fc7689f76705
Matheus Souza <63678546+mzsmatheus@users.noreply.github.com>
2022-05-20 19:50:40 -0300

    Atualizado controller treinamento

--- human-resources/src/main/java/edu/up/br/projeto_dev_software/controllers/controller_treinamento/ListaController.java
+++ human-resources/src/main/java/edu/up/br/projeto_dev_software/controllers/controller_treinamento/ListaController.java
@@ -1,61 +1,84 @@
 package edu.up.br.projeto_dev_software.controllers.controller_treinamento;
 
-import org.springframework.web.bind.annotation.RestController;
-
-import edu.up.br.projeto_dev_software.repository.ListaRepository;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import edu.up.br.projeto_dev_software.domain.domain_treinamento.*;;
-
+import edu.up.br.projeto_dev_software.domain.domain_cadastro_funcionario.Funcionario;
+import edu.up.br.projeto_dev_software.domain.domain_treinamento.Lista;
+import edu.up.br.projeto_dev_software.domain.domain_treinamento.TreinamentoFuncionario;
+import edu.up.br.projeto_dev_software.repository.FuncionarioRepository;
+import edu.up.br.projeto_dev_software.repository.ListaRepository;;
 
 @RestController
+@RequestMapping("/treinamento")
 public class ListaController {
 
-    private final ListaRepository repository;
+    @Autowired
+    private ListaRepository treinamentoRP;
 
-    ListaController(ListaRepository repository){
-        this.repository = repository;
+    @Autowired
+    private FuncionarioRepository funcionarioRP;
+
+    @GetMapping("/listar")
+    Iterable<Lista> listar() {
+        System.out.println("Listar");
+        return treinamentoRP.findAll();
+
     }
 
-    @GetMapping("/listas")
-    Iterable<Lista> listar(){
-        return repository.findAll();
-
+    @GetMapping("/listar/{id}")
+    Lista buscarPorId(@PathVariable Long id) {
+        return treinamentoRP.findById(id).get();
     }
 
-    @GetMapping("/listas/{id}")
-    Lista buscarPorId(@PathVariable Long id){
-        return repository.findById(id).get();
+    @PostMapping("/incluir")
+    Lista incluir(@RequestBody Lista novaLista) {
+        return treinamentoRP.save(novaLista);
     }
     
-    @PostMapping("/listas")
-    Lista incluir(@RequestBody Lista novaLista){
-        return repository.save(novaLista);
+    @PutMapping("/atualizar/{id}")
+    Lista atualizar(@RequestBody Lista listaAlterada, @PathVariable Long id) {
+        return treinamentoRP.findById(id)
+                .map(lista -> {
+                    lista.setNomeTreinamento(listaAlterada.getNomeTreinamento());
+                    return treinamentoRP.save(lista);
+                })
+
+                .orElseGet(() -> {
+                    listaAlterada.setId(id);
+                    return treinamentoRP.save(listaAlterada);
+                });
     }
 
-    @PutMapping("/listas/{id}")
-    Lista atualizar(@RequestBody Lista listaAlterada, @PathVariable Long id){
-        return repository.findById(id)
-        .map(lista -> {
-            lista.setNome(listaAlterada.getNome());
-            return repository.save(lista);
-        })
-
-        .orElseGet(() -> {
-            listaAlterada.setId(id);
-            return repository.save(listaAlterada);
-        });    
+    @DeleteMapping("/deletar/{id}")
+    void excluir(@PathVariable Long id) {
+        treinamentoRP.deleteById(id);
     }
 
-    @DeleteMapping("/listas/{id}")
-    void excluir(@PathVariable Long id){
-        repository.deleteById(id);
+    //Parte de incluir um funcionario a um treinamento
+
+    @PostMapping("/incluirFuncionario")
+    Lista incluirFuncionario(@RequestBody TreinamentoFuncionario treinamentoFuncionario) {
+        Lista treinamento = treinamentoRP.findById(treinamentoFuncionario.getId_treinamento()).get();
+        Funcionario funcionario = funcionarioRP.findById(treinamentoFuncionario.getId_funcionario()).get();
+        treinamento.getFuncionarios().add(funcionario);
+        
+        return treinamentoRP.save(treinamento);
     }
+
+    //Parte de tirar um funcionario de um treinamento
+    // @PostMapping("/removerFuncionario")
+    // Lista removerFuncionario(@RequestBody TreinamentoFuncionario treinamentoFuncionario){
+    //     Lista treinamento = treinamentoRP.findById(treinamentoFuncionario.getId_treinamento()).get();
+    //    // treinamento.getFuncionarios()
+    //     return treinamento;
+    // }
 
 }
