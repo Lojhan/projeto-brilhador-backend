061a4b865566fae43f068cc6bfe8e6a319f980cd
Matheus Souza <63678546+mzsmatheus@users.noreply.github.com>
2022-05-19 22:14:28 -0300

    Atualizado Repository treinamento

--- human-resources/src/main/java/edu/up/br/projeto_dev_software/repository/ListaRepository.java
+++ human-resources/src/main/java/edu/up/br/projeto_dev_software/repository/ListaRepository.java
@@ -1,10 +1,9 @@
 package edu.up.br.projeto_dev_software.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.repository.CrudRepository;
 import edu.up.br.projeto_dev_software.domain.domain_treinamento.Lista;
 
 
 
-public interface ListaRepository extends JpaRepository<Lista, Long> {
+public interface ListaRepository extends CrudRepository<Lista, Long> {
     
 }
