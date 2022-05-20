97c595e53d0f2236cf81206b0fde44ace903bab5
Matheus Souza <63678546+mzsmatheus@users.noreply.github.com>
2022-05-19 22:13:29 -0300

    Delete human-resources/src/main/java/edu/up/br/projeto_dev_software/domain/repository directory

--- human-resources/src/main/java/edu/up/br/projeto_dev_software/domain/repository/ListaRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.up.br.projeto_dev_software.repository;
-import org.springframework.data.repository.CrudRepository;
-import edu.up.br.projeto_dev_software.domain.domain_treinamento.Lista;
-
-
-
-public interface ListaRepository extends CrudRepository<Lista, Long> {
-    
-}
