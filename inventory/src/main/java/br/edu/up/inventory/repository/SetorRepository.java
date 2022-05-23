aa124d7d9255548835de449e03809b5cd47d5f97
nicole branco <hyaluna@gmail.com>
2022-05-23 20:41:43 -0300

    add setor

---
+++ inventory/src/main/java/br/edu/up/inventory/repository/SetorRepository.java
@@ -0,0 +1,9 @@
+package br.edu.up.inventory.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.edu.up.inventory.domain.Setor;
+
+public interface SetorRepository extends CrudRepository<Setor, Long> {
+
+}
