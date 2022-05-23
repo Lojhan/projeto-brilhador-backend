966f0e447a47155ccc81ebc37ef87c3e28ec6d72
nicole branco <hyaluna@gmail.com>
2022-05-23 20:42:00 -0300

    add material

---
+++ inventory/src/main/java/br/edu/up/inventory/domain/Material.java
@@ -0,0 +1,101 @@
+package br.edu.up.inventory.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Material {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String name;
+    private int validade;
+    private int quantidade;
+    private String unidade;
+
+    @Column(name = "id_setor")
+    private long idSetor;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="id_setor", updatable = false, insertable = false)
+    private Setor setor;
+
+    public Material() {
+    }
+
+    public Material(long id, String name, int validade, int quantidade, String unidade) {
+        this.id = id;
+        this.name = name;
+        this.validade = validade;
+        this.quantidade = quantidade;
+        this.unidade = unidade;
+    }
+
+    public Material(String name, int validade, int quantidade, String unidade) {
+        this.name = name;
+        this.validade = validade;
+        this.quantidade = quantidade;
+        this.unidade = unidade;
+    }
+
+    public Material(long id, String name, int validade) {
+        this.id = id;
+        this.name = name;
+        this.validade = validade;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUnidade() {
+        return unidade;
+    }
+
+    public void setUnidade(String unidade) {
+        this.unidade = unidade;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public int getValidade() {
+        return validade;
+    }
+
+    public void setValidade(int validade) {
+        this.validade = validade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void atualizarMaterial(Material materialAtualizado) {
+        this.name = materialAtualizado.getName();
+        this.validade = materialAtualizado.getValidade();
+        this.quantidade = materialAtualizado.getQuantidade();
+        this.unidade = materialAtualizado.getUnidade();
+    }
+
+    public long getIdSetor() {
+        return idSetor;
+    }
+
+    public void setIdSetor(long idSetor) {
+        this.idSetor = idSetor;
+    }
+}
