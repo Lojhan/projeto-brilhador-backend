681465454a31925a58f41f69766c323f1a062ea5
Lucas Volkmann <Zionlks@gmail.com>
2022-05-24 00:08:06 -0300

    remove produto

--- inventory/src/main/java/br/edu/up/inventory/repository/ProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.edu.up.inventory.repository;
-
-import br.edu.up.inventory.domain.Produto;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProdutoRepository extends CrudRepository<Produto, Long> {
-}
