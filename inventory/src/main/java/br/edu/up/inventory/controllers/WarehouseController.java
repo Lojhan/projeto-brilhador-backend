c5bc69a44da1dfe6ecc2d6df8900d0b2bc67c425
Aramis Chang Chain <63321839+Aramischangchain@users.noreply.github.com>
2022-05-23 23:36:32 -0300

    Create WarehouseController.java

---
+++ inventory/src/main/java/br/edu/up/inventory/controllers/WarehouseController.java
@@ -0,0 +1,52 @@
+package br.edu.up.inventory.controllers;
+
+import br.edu.up.inventory.domain.Warehouse;
+import br.edu.up.inventory.repository.WarehouseRepository;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/warehouse")
+public class WarehouseController {
+
+    private final WarehouseRepository repository;
+
+    WarehouseController(WarehouseRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping()
+    Iterable<Warehouse> list() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    Warehouse findById(@PathVariable Long id) {
+        return repository.findById(id).get();
+    }
+
+    @PostMapping()
+    Warehouse include(@RequestBody Warehouse newWarehouse) {
+        return repository.save(newWarehouse);
+    }
+
+    @PutMapping("/{id}")
+    Warehouse update(@RequestBody Warehouse warehouseChanged, @PathVariable Long id) {
+        return repository.findById(id)
+                .map(warehouse -> {
+                    // id encontrado
+                    warehouse.updateWarehouse(warehouseChanged);
+                    return repository.save(warehouse);
+                })
+                .orElseGet(() -> {
+                    // id não encontrado
+                    warehouseChanged.setId(id);
+                    return repository.save(warehouseChanged);
+                });
+    }
+
+    @DeleteMapping("/{id}")
+    void delete(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+
+}
