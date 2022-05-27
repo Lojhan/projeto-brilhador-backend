2d2ce2a4eaa930b8a79cc50208493db392962283
nicole branco <hyaluna@gmail.com>
2022-05-27 16:55:58 -0300

    fix

--- inventory/src/main/java/br/edu/up/inventory/controllers/MovementController.java
+++ inventory/src/main/java/br/edu/up/inventory/controllers/MovementController.java
@@ -58,7 +58,6 @@ public class MovementController {
                 newQuantity
         );
 
-        // create a client
         HttpClient client = HttpClient.newHttpClient();
 
         HttpRequest request = HttpRequest.newBuilder()
