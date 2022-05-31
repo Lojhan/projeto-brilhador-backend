78aa4bd1e883a1be28da89515a3345af510cb0e9
nicole branco <hyaluna@gmail.com>
2022-05-30 23:11:59 -0300

    handle product allocation automatically

--- inventory/src/main/java/br/edu/up/inventory/domain/Product.java
+++ inventory/src/main/java/br/edu/up/inventory/domain/Product.java
@@ -38,10 +38,9 @@ public class Product {
     public void updateProduct(Product product) {
         this.price = product.getPrice();
         this.name = product.getName();
-        this.quantity = product.getQuantity();
         this.description = product.getDescription();
         this.sku = product.getSku();
-        this.idWarehouse = product.getIdWarehouse();
+        // quantity and idWarehouse should only be modified when creating a Movement
     }
 
     public long getId() {
