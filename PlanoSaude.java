d6bdac42c54f13356146dd04a952ccdc627daba2
Naiara de Mello Martins <44578089+NaiaraMM@users.noreply.github.com>
2022-06-01 19:45:17 -0300

    commit atualizacao

---
+++ PlanoSaude.java
@@ -0,0 +1,46 @@
+package edu.up.br.projeto_dev_software.domain.domain_plano_saude;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "tb_plano_Saude") // efetuado
+public class PlanoSaude{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nomePlano;
+    private Double valorPlano;
+
+    public PlanoSaude(){        
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomePlano() {
+        return nomePlano;
+    }
+
+    public void setNomePlano(String nomePlano) {
+        this.nomePlano = nomePlano;
+    }
+
+    public Double getValorPlano() {
+        return valorPlano;
+    }
+
+    public void setValorPlano(Double valorPlano) {
+        this.valorPlano = valorPlano;
+    }
+}
