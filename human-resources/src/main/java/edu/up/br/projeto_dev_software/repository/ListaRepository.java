10cd88de04b1fb8450e865d116fe08d8a5e794e3
Matheus Z. e Souza <63678546+mzsmatheus@users.noreply.github.com>
2022-06-03 19:33:23 -0300

    Atualizado comentarios com atributos
    Foram comentados exemplos da estrutura da classe caso necessário o uso de json server.

--- human-resources/src/main/java/edu/up/br/projeto_dev_software/repository/ListaRepository.java
+++ human-resources/src/main/java/edu/up/br/projeto_dev_software/repository/ListaRepository.java
@@ -8,3 +8,12 @@ import edu.up.br.projeto_dev_software.domain.domain_treinamento.Lista;
 public interface ListaRepository extends JpaRepository<Lista, Long> {
     
 }
+
+  // Estrutura e exemplos lista de treinamento para json server:
+  // ID: 1
+  // Nome Treinamento: Treinamento funcionarios do setor de Estoque
+  // Area (FK vindo da classe Funcionario): "Estoque"
+
+  // ID: 2
+  // Nome Treinamento: Treinamento funcionarios do setor Financeiro
+  // Area (FK vindo da classe Funcionario): "Financeiro"
