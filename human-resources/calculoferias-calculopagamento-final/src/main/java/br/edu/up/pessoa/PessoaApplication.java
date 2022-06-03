35b222ea027498d997e9887db7b50aa1ae84d634
Thiago de Lima <thiago_loukura@hotmail.com>
2022-06-03 15:28:01 -0300

    inclusao autenticacao para acessar api

--- human-resources/calculoferias-calculopagamento-final/src/main/java/br/edu/up/pessoa/PessoaApplication.java
+++ human-resources/calculoferias-calculopagamento-final/src/main/java/br/edu/up/pessoa/PessoaApplication.java
@@ -1,12 +1,14 @@
 package br.edu.up.pessoa;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
+@EnableAutoConfiguration
 public class PessoaApplication {
 
 	public static void main(String[] args) {
