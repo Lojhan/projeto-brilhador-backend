03966de9dadce27d7f83f7d24476abe7d68ceffb
Milena Milicio <milenamilicio@hotmail.com>
2022-06-08 16:00:30 -0300

    inventory: adiciona health check

--- inventory/src/main/java/br/edu/up/inventory/InventoryApplication.java
+++ inventory/src/main/java/br/edu/up/inventory/InventoryApplication.java
@@ -3,7 +3,9 @@ package br.edu.up.inventory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class InventoryApplication {
 
