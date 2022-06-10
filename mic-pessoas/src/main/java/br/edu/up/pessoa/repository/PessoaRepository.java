60312a59bb1f1699aff1f7003162f59d024278ef
fernando.rezende <luis.santos@tuntscorp.com>
2022-06-10 15:43:30 -0300

    fixing encoding bug

--- mic-pessoas/src/main/java/br/edu/up/pessoa/repository/PessoaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.edu.up.pessoa.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.edu.up.pessoa.domain.Pessoa;
-
-public interface PessoaRepository extends CrudRepository<Pessoa, Long> {
-    
-}
--- mic-pessoas/src/main/java/br/edu/up/pessoa/venda/repository/VendaRepository.java
+++
@@ -1,8 +0,0 @@
-package br.edu.up.pessoa.venda.repository;
-
-import br.edu.up.pessoa.venda.domain.Venda;
-import org.springframework.data.repository.CrudRepository;
-
-public interface VendaRepository extends CrudRepository<Venda, Long> {
-    
-}
